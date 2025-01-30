0aa60ca10617ae9431128b3e3e12227ae889f4e3
Anathos77 <maximebro1@gmail.com>
2025-01-30 02:37:02 +0100

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,7 +3,7 @@ const { DistributionAPI } = require('helios-core/common')
 const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'https://bedragoth.strator.gg/nebula/distribution.json'
+// exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/Anathos77/Test/master/distribution.json'
 exports.REMOTE_DISTRO_URL = 'https://bedragoth.strator.gg/nebula/distribution.json'
 
 const api = new DistributionAPI(
